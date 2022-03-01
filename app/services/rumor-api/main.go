ce9c536ea1ca7e4b45334612a0b497ed61b23f5c
Jan Duursma <me@janduursma.com>
2022-02-28 23:13:42 -0800

    Add logging support

--- app/services/rumor-api/main.go
+++ app/services/rumor-api/main.go
@@ -1,30 +1,70 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 
+	"go.uber.org/zap/zapcore"
+
 	"github.com/emadolsky/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 )
 
 var build = "develop"
 
 func main() {
+	// Construct the application logger.
+	log, err := initLogger("RUMOR-API")
+	if err != nil {
+		log.Fatalf("error constructing logger: %s", err)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// Check what maxprocs reports.
+	opt := maxprocs.Logger(log.Infof)
+
 	// Set the correct number of threads for the rumor-api
 	// based on what is available either by the machine or quotas.
-	if _, err := maxprocs.Set(); err != nil {
-		log.Fatalf("error setting maxprocs: %s", err)
+	if _, err := maxprocs.Set(opt); err != nil {
+		return fmt.Errorf("error setting maxprocs: %w", err)
 	}
 
-	log.Printf("starting rumor-api, build[%s] CPU[%d]", build, runtime.GOMAXPROCS(0))
-	defer log.Println("rumor-api ended")
+	log.Infow("starting rumor-api", "version", build, "CPU", runtime.GOMAXPROCS(0))
+	defer log.Infow("shutdown completed")
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	log.Println("stopping rumor-api")
+	log.Infow("shutting down rumor-api")
+
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,10 @@ go 1.17
 
 require (
 	github.com/emadolsky/automaxprocs v1.4.1-0.20220104203543-349281033d17
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 )
